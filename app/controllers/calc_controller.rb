b3802bfaf040a68f5e4b9455e9029a80fdb3efa6
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-06-04 16:09:36 +0000

    square root results route

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -13,4 +13,12 @@ class CalcController > ApplicationController
   def new_squareroot 
     render({ :template => "pages_templates/new_squareroot"})
   end
+
+  def squareroot_results
+    @the_num = params.fetch("user_number").to_f
+    @the_result = Math.sqrt(@the_num)
+
+    render({ :template => "pages_templates/squareroot_results"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   get("/square/results" , { :controller => calc , :action => square_results })
   
-  get("/square_root/new", { :controller => calc , :action => new_squareroot })
+  get("/square_root/new" , { :controller => calc , :action => new_squareroot })
+
+  get("/square_root/results" , { :controller => calc , :action => squareroot_results })
     
 
   

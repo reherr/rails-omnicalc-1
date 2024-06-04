d4069ec1d6a302bf5dca20dbcdfe1c60d27dffd8
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-06-04 16:22:19 +0000

    added quotation marks for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
 
-  get("/square/new" , { :controller => calc , :action => new_square })
+  get("/square/new" , { :controller => "calc" , :action => "new_square" })
 
-  get("/square/results" , { :controller => calc , :action => square_results })
+  get("/square/results" , { :controller => "calc" , :action => "square_results" })
   
-  get("/square_root/new" , { :controller => calc , :action => new_squareroot })
+  get("/square_root/new" , { :controller => "calc" , :action => "new_squareroot" })
 
-  get("/square_root/results" , { :controller => calc , :action => squareroot_results })
+  get("/square_root/results" , { :controller => "calc" , :action => "squareroot_results" })
 
-  get("/payment/new" , { :controller => calc , :action => payment_new })
+  get("/payment/new" , { :controller => "calc" , :action => "payment_new" })
   
-  get("/payment/results" , { :controller => calc , :action => payment_results})
+  get("/payment/results" , { :controller => "calc" , :action => "payment_results"})
 
-  get("/random/new" , { :controller => calc , :action => random_new})
+  get("/random/new" , { :controller => "calc" , :action => "random_new"})
   
-  get("/random/results" , { :controller => calc , :action => random_results})
+  get("/random/results" , { :controller => "calc" , :action => "random_results"})
 
 end
